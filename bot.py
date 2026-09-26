2c2fbad7bcbca64185229af6120efab23363adea
bbjybbjysktshwyr-cell <bbjybbjysktshwyr@gmail.com>
2026-09-26 17:13:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 
-TOKEN = "8860565104:AAEVEX4ODFumP981Sto89sCZZmOe7MSHtzU"
+TOKEN = "8618789887:AAGKxnDN6a0ulOS9aLyB1HnuNygukFsIVHs"
 ADMIN_ID = 6697426766
 API_URL = "http://127.0.0.1:2233/delta"
 
